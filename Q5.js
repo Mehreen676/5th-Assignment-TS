b94882facdc7498822d365dc6918274a14af70a6
Mehreen676 <159542833+Mehreen676@users.noreply.github.com>
2024-02-23 21:06:06 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.Then compose your message and store it in a new variable called message.Print your message.
+let famouse_person = "Umar Ibn Al-Khattab ";
+let message = " “When one's intention is sincere, God will suffice his needs, protect him, and guide him in his dealings with the people.” ";
+console.log(`${famouse_person} once said,${message}`);
---
+++ Q5.ts
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.Then compose your message and store it in a new variable called message.Print your message.
+let famouse_person:string ="Umar Ibn Al-Khattab "
+let message:string =" “When one's intention is sincere, God will suffice his needs, protect him, and guide him in his dealings with the people.” "
+console.log(`${famouse_person} once said,${message}`)
